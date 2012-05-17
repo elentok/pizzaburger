1fccc94626ed9da54226b17fdf9b94fa4a4b6bb1
David Elentok <3david@gmail.com>
2012-05-17 11:57:19 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pizzaburger::Application.config.secret_token = '12a366f0850f0f16345b87a77e44e08b31972e9bf5873e7b6772e23162db3d34dc87d1975d8525d20063eec8907dfb0179441cd4fefef83ed0f3f271cb1f5349'
